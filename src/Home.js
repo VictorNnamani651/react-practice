a33e84180be85e7ba43758e68931560694ef9fa0
Nnamani Victor <victornnamani651@gmail.com>
2023-02-15 04:38:30 +0100

    deleted few unwanted files and modified a few remaining ones

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+function Home() {
+  let text = "This Is The Home Page";
+  return (
+    <div className="body">
+      <h2>{text}</h2>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/Nav.js
@@ -0,0 +1,13 @@
+function Nav() {
+  return (
+    <div className="nav-bar">
+      <h1>This Is The Nav Bar</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/create">New Blog</a>
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
