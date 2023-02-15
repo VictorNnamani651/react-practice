1f44bdac45839bd4bfce89eb0d1f6188fc065e12
Nnamani Victor <victornnamani651@gmail.com>
2023-02-15 04:58:10 +0100

    ...

--- src/Nav.js
+++ src/Nav.js
@@ -1,7 +1,7 @@
 function Nav() {
   return (
     <div className="nav-bar">
-      <h1>This Is The Nav Bar!!</h1>
+      <h1>This Is The Nav Bar!!!!!!</h1>
       <div className="links">
         <a href="/">Home</a>
         <a href="/create">New Blog</a>
