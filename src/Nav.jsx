94cd15cee3c1cc7f0ec17e3e2e63e162e2c6e4fb
Nnamani Victor <victornnamani651@gmail.com>
2023-03-19 09:55:44 +0100

    added buttons and icons to the nav bar
    i turned what was formerly just an icon to a button for the nav bar menu button and also added a button to enable users close the menu, also made a few adjustments to the padding and positioning of elements within the the menu

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -2,11 +2,15 @@ function Nav() {
   return (
     <div className="nav-bar">
       <h1>The Nav Bar</h1>
-
-      <i class="fa fa-bars side-menu-btn mobile-view" aria-hidden="true"></i>
-
+      <button className="side-menu-btn mobile-view">
+        <i class="fa fa-bars" aria-hidden="true"></i>
+      </button>
       <div className="links desktop-vie">
-        
+        <div className="close-btn-container mobile-view">
+          <button className="side-menu-close-btn">
+            <span class="material-symbols-outlined">close</span>
+          </button>
+        </div>
         <a href="/">Home</a>
         <a href="/create">New Blog</a>
       </div>
