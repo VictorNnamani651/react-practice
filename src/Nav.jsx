86cddc6119b5faa20c472deb4f2afb6edcd0c697
Nnamani Victor <victornnamani651@gmail.com>
2023-03-20 06:17:00 +0100

    added useState hook and added animation
    i imported useState to enable users change the display state of the side menu for the mobile view and i also added an entrance animation for the side menu

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,14 +1,25 @@
+import { useState } from "react";
+
 function Nav() {
+  let [display, setDisplay] = useState(
+    "links hide"
+  );
+  let displayStatus = () => {
+    display === "links hide"
+      ? setDisplay("links animate__animated animate__fadeIn")
+      : setDisplay("links hide");
+  };
+
   return (
     <div className="nav-bar">
       <h1>The Nav Bar</h1>
-      <button className="side-menu-btn mobile-view">
-        <i class="fa fa-bars" aria-hidden="true"></i>
+      <button className="side-menu-btn mobile-view" onClick={displayStatus}>
+        <i className="fa fa-bars" aria-hidden="true"></i>
       </button>
-      <div className="links desktop-vie">
+      <div className={display}>
         <div className="close-btn-container mobile-view">
-          <button className="side-menu-close-btn">
-            <span class="material-symbols-outlined">close</span>
+          <button className="side-menu-close-btn" onClick={displayStatus}>
+            <span className="material-symbols-outlined">close</span>
           </button>
         </div>
         <a href="/">Home</a>
